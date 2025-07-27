4a744553b4d7dd2e6e5c44c2f096adce51a3eb44
Mahbub <mahbub.buet.cse18@gmail.com>
2025-07-27 16:25:18 +0600

    Components and Pages are created

---
+++ stayswift/app/login/page.js
@@ -0,0 +1,14 @@
+import LoginForm from "@/components/auth/LoginForm";
+import SocialLogins from "@/components/auth/SocialLogins";
+
+export default function LoginPage() {
+  return (
+    <section class="h-screen grid place-items-center">
+      <div class="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
+        <h4 class="font-bold text-2xl">Sign in</h4>
+        <LoginForm />
+        <SocialLogins />
+      </div>
+    </section>
+  );
+}
