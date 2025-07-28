1164e2218a663b5abd7b042fdf84fd7ea9d071bc
Mahbub <mahbub.buet.cse18@gmail.com>
2025-07-29 02:14:31 +0600

    login with google

---
+++ stayswift/auth.js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import mongoClientPromise from "./database/mongoClientPromise";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: MongoDBAdapter(mongoClientPromise,{databaseName:process.env.ENVIRONMENT}),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
